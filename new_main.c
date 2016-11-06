6ed28ac5a398a434aa09fa3031d3ce0c6578a720
Matthew Gould <matt@everettgould.com>
2016-11-05 21:18:31 -0700

    validate function

--- new_main.c
+++ new_main.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 18:35:04 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/05 18:40:22 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/05 21:08:42 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 #include <unistd.h>
 #include "make_matrix.c"
 #include "print_matrix.c"
+#include "validate_input.c"
 
 int main(int argc, char **argv)
 {
-	argc = 0;
+	if (validate_input(argc, argv))
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
 	print_matrix(make_matrix(argv));
 
 	return (0);
