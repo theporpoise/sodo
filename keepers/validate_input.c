e4724e7572e83a10b6364cabc0e8c0af33964076
Matthew Gould <matt@everettgould.com>
2016-11-06 07:46:11 -0800

    finally got the validator working, had an off by one error in argc and the or statement out of parenthesis

--- keepers/validate_input.c
+++ keepers/validate_input.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 18:49:03 by mgould            #+#    #+#             */
-/*   Updated: 2016/11/05 21:17:52 by mgould           ###   ########.fr       */
+/*   Updated: 2016/11/06 07:36:48 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,23 @@ int	validate_input(int argc, char **argv)
 {
 	int i;
 	int j;
-	
+
 	i = 1;
 	j = 0;
-	while (i < (argc - 1))
+	while (i < argc)
 	{
 		while (argv[i][j])
 		{
-			if	(!(argv[i][j] == '.' || (argv[i][j] >= '1' && argv[i][j] <= '9')))
+			if (!((argv[i][j] == '.')
+				|| (argv[i][j] >= '1' && argv[i][j] <= '9')))
 			{
-				printf("argiv is:%c", argv[i][j]);
-				printf("j is:%d\n", j);
 				return (1);
 			}
 			else if (j > 8)
-			{
-				printf("j greater than 8\n");
 				return (1);
-			}
 			j++;
 		}
 		if (j != 9)
-			printf("j is %d", j);
 			return (1);
 		i++;
 		j = 0;
